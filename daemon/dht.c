56ac1f3a71793e6557c765b2785a84310b1cc662
Serge Shu <shubinov@gmail.com>
2019-11-03 14:47:39 +0500

    added WiringOP submodule

--- daemon/dht.c
+++ daemon/dht.c
@@ -4,13 +4,72 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/types.h>         
- 
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1 
 #define MAX_TIMINGS 85
 #define DHT_PIN 7
+
 int data[5] = { 0, 0, 0, 0, 0 };
 int fd[2];
 FILE *th_file;
 char *f_name = "/tmp/tmp_hmd.json";
+
+//daemonize process
+static void daemonize(void)
+{
+    pid_t pid, sid;
+    int fds;
+    /* already a daemon */
+    if ( getppid() == 1 ) return;
+
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0)  
+    {   
+        exit(EXIT_FAILURE);
+    }   
+
+    if (pid > 0)  
+    {   
+        exit(EXIT_SUCCESS); /*Killing the Parent Process*/
+    }   
+
+    /* At this point we are executing as the child process */
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0)  
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory. */
+    if ((chdir("/")) < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+
+    fds = open("/dev/null",O_RDWR, 0);
+
+    if (fds != -1)
+    {
+        dup2 (fds, STDIN_FILENO);
+        dup2 (fds, STDOUT_FILENO);
+        dup2 (fds, STDERR_FILENO);
+
+        if (fds > 2)
+        {
+            close (fds);
+        }
+    }
+
+    /*resettign File Creation Mask */
+    umask(022);
+}
 
 void read_dht_data()
 {
@@ -92,15 +151,18 @@ void read_dht_data()
  
 int main( void )
 {
-    printf( "DHT22 temperature/humidity test\n" );
-	 
+    //printf( "DHT22 temperature/humidity test\n" );
+
     if ( wiringPiSetup() == -1 )
         exit( 1 );
+
+    daemonize();
  
     while ( 1 )
     {
         read_dht_data();
-        delay( 2000 ); /* wait 2 seconds before next read */
+        //delay( 2000 ); /* wait 2 seconds before next read */
+        sleep(2);
     }
 
     return(0);
